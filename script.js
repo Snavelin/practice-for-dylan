a0b2d49b94d43688af3aa2c32e2b6ad95dc0f070
Snavelin <snavelyds@gmail.com>
2020-07-12 12:16:26 -0700

    Added function to close modal when user clicks off it.

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-var goLiveModal = document.getElementsByClassName("goLiveModal")[0]
-// // Get button that opens modal.
+var goLiveModal = document.getElementsByClassName("goLiveModal")[0];
+// Get button that opens modal.
 var goLiveButton = document.getElementById("goLiveButton");
-// Set click function that opens modal.
 var main = document.getElementsByClassName("main")[0];
-goLiveButton.onclick = function() {
-    goLiveModal.style.display = "flex";
+var modalContainer = document.getElementsByClassName("modalContainer")[0];
+// Set click function that opens modal.
+goLiveButton.onclick = function () {
     main.style.opacity = "10%";
+    modalContainer.style.display = "flex";
+}
+// When user clicks outside of modal, close it.
+window.onclick = function (event) {
+    if (event.target == modalContainer) {
+        main.style.opacity = "100%";
+        modalContainer.style.display = "none";
+    }
 }
